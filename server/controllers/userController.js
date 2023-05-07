5fd9e358db33c808454be16fbbaa2d7d490bcbc5
Archangxl <lixo.mueller@gmail.com>
2023-05-06 23:43:04 -0600

    last updated 05.06.2023

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,54 +1,46 @@
 const User = require('../models/userModel');
-const RecipeController = require('../controllers/recipeController');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
-
-    findAllUsers: (req,res) => {
-        User.find().sort({"firstName": 'asc'})
-            .then(allUsers => {
-                res.status(200).json(allUsers);;
-            })
-            .catch(err => {
-                res.status(400).json(err);
-            })
-    },
 
     registerUser: (req, res) => {
         User.create(req.body)
             .then(user => {
                 const userToken = jwt.sign({
                     id: user._id
-                }, process.env.Kitchen);
+                }, process.env.FIRST_SECRET_KEY);
 
                 res.cookie("userToken", userToken, { httpOnly: true }).json({
                     message: "This response has a cookie", user: user
                 });
             })
             .catch(err => {
-                res.status(400).json(err);
+                res.status(400).json({err: err, message: "Something went wrong!"});
             })
     },
 
     login: async(req, res) => {
-        const user = await User.findOne({_id: req.body.email});
+        const user = await User.findOne({email: req.body.email});
         
         if (user === null) {
             return res.status(400).json({message: "User doesn't exits!"});
         }
-
+        
         const passwordInDB = await bcrypt.compare(req.body.password, user.password);
 
-        if (!passwordInDB) {
-            return res.sendStatus(400).json({message: "Password is incorrect!"});
+        if (passwordInDB === false) {
+            return res.status(400).json({message: "Password is incorrect!"});
         }
-
+        
         const userTokenForCookies = jwt.sign({
             id: user._id
-        }, process.env.Kitchen);
+        }, process.env.FIRST_SECRET_KEY);
 
         res.cookie("userToken", userTokenForCookies, {
             httpOnly: true
-        }).json({msg: success})
+        }).json({msg: "success"})
+        
     },
 
     updateUser: (req, res) => {
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const UserController = require('../controllers/userController');
 
 module.exports = app => {
-    app.get('/api/allUsers', UserController.findAllUsers);
+    app.get('/api/logout', UserController.logout);
     app.post('/api/registerUser', UserController.registerUser);
-    app.get('/api/grabUser/:id', UserController.findOneUser);
+    app.get('/api/login', UserController.login);
     app.put('/api/updateUser/:id', UserController.updateUser);
     app.delete('/api/deleteUser/:id', UserController.deleteUser);
 }
