98fede800e1b322bb3ed714422efa6a82d711a08
Alexander <lixo.mueller@gmail.com>
2023-05-07 23:33:06 -0600

    last updated 05.07.2023

--- client/src/Components/LoginRegistration.js
+++ client/src/Components/LoginRegistration.js
@@ -3,89 +3,45 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const LoginRegistration = () => {
-    const [registerLoaded, setRegisterLoaded] = useState(false);
     // Register process, login process below "//End of register Process"
-    const [registeringUser] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        confirmPassword: ""
-    })
-    const [registerErrors] = useState({
-        firstNameError: "",
-        lastNameError: "",
-        emailError: "",
-        passwordError: "",
-        confirmPasswordError: "",
-        hasTheFormBeenSubmitted: 
-    })
+    const [firstName, setFirstName] = useState();
+    const [firstNameError, setFirstNameError] = useState();
+    const [lastName, setLastName] = useState();
+    const [lastNameError, setLastNameError] = useState();
+    const [email, setEmail] = useState();
+    const [emailError, setEmailError] = useState();
+    const [password, setPassword] = useState();
+    const [passwordError, setPasswordError] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
+    const [confirmPasswordError, setConfirmPasswordError] = useState();
+
+    const navigate = useNavigate();
 
     const onRegisterSubmittion = async(e) => {
         e.preventDefault();
         try {
-            const registering = await axios.post('http://localhost:8000/api/registerUser', registeringUser);
-            console.log(registering.data);
+            const registering = await axios.post('http://localhost:8000/api/registerUser', 
+            {firstName: firstName, lastName: lastName, email: email, password: password, confirmPassword: confirmPassword} );
+            setFirstName();
+            setLastName();
+            setEmail();
+            setPassword();
+            setConfirmPassword();
 
-            registeringUser.firstName = "";
-            registeringUser.lastName = "";
-            registeringUser.email = "";
-            registeringUser.password = "";
-            registeringUser.confirmPassword = "";
-
-            registerErrors.firstNameError = "";
-            registerErrors.lastNameError = "";
-            registerErrors.emailError = "";
-            registerErrors.passwordError = "";
-            registerErrors.confirmPasswordError = "";
+            console.log(registering);
+            navigate('/')
             
         } catch(err) {
-            let errResponse = err.response.data;
-
-            if (errResponse.err.errors.firstName === undefined) {
-                registerErrors.firstNameError = undefined;
-            } else {
-                registerErrors.firstNameError = errResponse.err.errors.firstName.message;
-            }
-            if (errResponse.err.errors.lastName === undefined) {
-                registerErrors.lastNameError = undefined;
-            } else {
-                registerErrors.lastNameError = errResponse.err.errors.lastName.message;
-            }
-            if (errResponse.err.errors.email === undefined) {
-                registerErrors.emailError = undefined;
-            } else {
-                registerErrors.emailError = errResponse.err.errors.email.message;
-            }
-            if (errResponse.err.errors.password === undefined) {
-                registerErrors.passwordError = undefined;
-            } else {
-                registerErrors.passwordError = errResponse.err.errors.password.message;
-            }
-            if (registeringUser.confirmPassword === registeringUser.password) {
-                registerErrors.confirmPasswordError = undefined;
-            } else {
-                registerErrors.confirmPasswordError = "Passwords need to Match!";
-            }
-
-            console.log(registerErrors);
+            console.log(err);
         }
     }
     //End of register Process
     //login Process
-    const [loggingInUser] = useState({
-        email: "",
-        password: ""
-    })
-    const [loggingInErrors] = useState({
-        emailError: "",
-        passwordError: ""
-    })
 
     const onLoggingSubmittion = async(e) => {
         e.preventDefault();
         try {
-            const findingUser = await axios.post('http://localhost:8000/api/login', loggingInUser);
+            const findingUser = await axios.post('http://localhost:8000/api/login', {email: email, password: password});
         } catch(err) {
             let searchingErrors = {emError: "", pasError: ""};
             let errResponse = err.response.data;
@@ -100,24 +56,24 @@ const LoginRegistration = () => {
                 <h1>My Cookbook</h1>
             </nav>
             <main>
-                { registeringErrors}
+                
                 <form onSubmit={onRegisterSubmittion}>
                     <h1>Register</h1>
-                    {registerErrors.firstNameError === undefined ? null : <p>{registerErrors.firstNameError}</p>}
+                    
                     <label>First Name: </label>
-                    <input onChange={(e) => registeringUser.firstName = (e.target.value)}></input>
-                    {registerErrors.lastNameError}
+                    <input value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
+                    
                     <label>Last Name: </label>
-                    <input onChange={(e) => registeringUser.lastName = (e.target.value)}></input>
-                    {registerErrors.emailError}
+                    <input value={lastName} onChange={(e) => setLastName(e.target.value)}></input>
+                    
                     <label>Email: </label>
-                    <input onChange={(e) => registeringUser.email = (e.target.value)}></input>
-                    {registerErrors.passwordError}
+                    <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                    
                     <label>Password: </label>
-                    <input onChange={(e) => registeringUser.password = (e.target.value)}></input>
-                    {registerErrors.confirmPasswordError}
+                    <input value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                    
                     <label>Confirm Password: </label>
-                    <input onChange={(e) => registeringUser.confirmPassword = (e.target.value)}></input>
+                    <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></input>
 
                     <button>Submit</button>
                 </form>
@@ -125,10 +81,10 @@ const LoginRegistration = () => {
                 <form onSubmit={onLoggingSubmittion}>
                     <h1>Log In</h1>
                     <label>Email: </label>
-                    <input onChange={(e) => loggingInUser.email = (e.target.value)}></input>
-
+                    <input onChange={(e) => setEmail(e.target.value)}></input>
+                    
                     <label>Password: </label>
-                    <input onChange={(e) => loggingInUser.password = (e.target.value)}></input>
+                    <input onChange={(e) => setPassword(e.target.value)}></input>
 
                     <button>Submit</button>
                 </form>
